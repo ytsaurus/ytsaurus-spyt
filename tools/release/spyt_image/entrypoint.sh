9ece1ec0b09e596aeb8397d35a92469a00c3f75f
atokarew <atokarew@ytsaurus.tech>
2024-05-29 12:21:36 +0300

    SPYT-637 Compat for older k8s operator 3147f6343a2c1e2a4c3ac7c71a35fe1d6ef92e73

--- tools/release/spyt_image/entrypoint.sh
+++ tools/release/spyt_image/entrypoint.sh
@@ -1,3 +1,12 @@
+
+# COMPAT(atokarew) setting default spark distrinb version to 3.2.2 if it not specified
+# Remove after k8s operator will be updated to explicitly specify spark versions
+if [ -z "$EXTRA_SPARK_VERSIONS" ]; then
+  EXTRA_SPARK_VERSIONS="3.2.2"
+fi
+# COMPAT end
+
+
 echo "EXTRA_CONFIG_GENERATOR_OPTIONS = $EXTRA_CONFIG_GENERATOR_OPTIONS"
 echo "EXTRA_PUBLISH_CLUSTER_OPTIONS = $EXTRA_PUBLISH_CLUSTER_OPTIONS"
 echo "EXTRA_SPARK_VERSIONS = $EXTRA_SPARK_VERSIONS"

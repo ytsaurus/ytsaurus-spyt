3151e3c3f9911d1744905909f270a162b3111626
atokarew <atokarew@ytsaurus.tech>
2024-06-18 15:05:48 +0300

    SPYT-662 YTsaurus client version up to 1.2.4 ab19094e66cf0707bf2db25443977bc31c3c8a7f

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val circeYamlVersion = "0.12.0"
   lazy val scalatestVersion = "3.1.0"
   lazy val livyVersion = "0.8.0-incubating"
-  lazy val ytsaurusClientVersion = "1.2.3"
+  lazy val ytsaurusClientVersion = "1.2.4"
   lazy val scalatraVersion = "2.7.0"
   lazy val mockitoVersion = "1.14.4"
   lazy val arrowVersion = "0.17.1"

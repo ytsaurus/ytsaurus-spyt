f8e9dead4b220721112a47094ef379dfe97edad3
atokarew <atokarew@ytsaurus.tech>
2024-09-10 13:17:38 +0300

    SPYT-698 Remove hotfix in AstBuilderSpyt and replace it with Decorate annotation e1ef7d45a0638b3bb1b3d1140e48e58d8cca7b13

--- data-source-extended/src/main/scala/org/apache/spark/sql/catalyst/parser/AstBuilderSpyt.scala
+++ data-source-extended/src/main/scala/org/apache/spark/sql/catalyst/parser/AstBuilderSpyt.scala
@@ -1,44 +1,37 @@
 package org.apache.spark.sql.catalyst.parser
 
-import org.apache.spark.sql.catalyst.parser.ParserUtils.{operationNotAllowed, withOrigin}
-import org.apache.spark.sql.catalyst.parser.SqlBaseParser.TablePropertyListContext
+import org.apache.spark.sql.catalyst.parser.AstBuilderSpyt.extractUint64Opt
+import org.apache.spark.sql.catalyst.parser.ParserUtils.withOrigin
 import org.apache.spark.sql.types.DataType
 import org.apache.spark.sql.spyt.types.UInt64Type
-import tech.ytsaurus.spyt.patch.annotations.{OriginClass, Subclass}
+import tech.ytsaurus.spyt.patch.annotations.{Decorate, DecoratedMethod, OriginClass, Subclass}
 
 import java.util.Locale
 
-@Subclass
+@Decorate
 @OriginClass("org.apache.spark.sql.catalyst.parser.AstBuilder")
-class AstBuilderSpyt extends AstBuilder {
+class AstBuilderSpyt {
 
-  // Hotfix: To prevent
-  // java.lang.NoSuchMethodError: 'void org.apache.spark.sql.catalyst.parser.AstBuilder$$anonfun$2.<init>
-  // (org.apache.spark.sql.catalyst.parser.AstBuilderBase)'
-  // In test YtSparkSQLTest::create table with custom attributes
-  override def visitPropertyKeyValues(ctx: TablePropertyListContext): Map[String, String] = {
-    val props = visitTablePropertyList(ctx)
-    for ((k, v) <- props) {
-      if (v == null) operationNotAllowed(s"Values must be specified for key: $k", ctx)
-    }
-    props
-  }
-
-  override def visitPrimitiveDataType(ctx: SqlBaseParser.PrimitiveDataTypeContext): DataType = {
-
-    val uint64Opt = withOrigin(ctx) {
-      val dataType = ctx.identifier.getText.toLowerCase(Locale.ROOT)
-      dataType match {
-        case "uint64" => Some(UInt64Type)
-        case _ => None
-      }
-    }
+  @DecoratedMethod
+  def visitPrimitiveDataType(ctx: SqlBaseParser.PrimitiveDataTypeContext): DataType = {
+    val uint64Opt = extractUint64Opt(ctx)
 
     if (uint64Opt.isDefined) {
       uint64Opt.get
     } else {
-      super.visitPrimitiveDataType(ctx)
+      __visitPrimitiveDataType(ctx)
     }
   }
 
+  def __visitPrimitiveDataType(ctx: SqlBaseParser.PrimitiveDataTypeContext): DataType = ???
+}
+
+object AstBuilderSpyt {
+  def extractUint64Opt(ctx: SqlBaseParser.PrimitiveDataTypeContext): Option[UInt64Type] = withOrigin(ctx) {
+    val dataType = ctx.identifier.getText.toLowerCase(Locale.ROOT)
+    dataType match {
+      case "uint64" => Some(UInt64Type)
+      case _ => None
+    }
+  }
 }

ee60e4244845b521468a36d725246979f46db883
mihailagei <mihailagei@ytsaurus.tech>
2025-02-10 17:46:58 +0300

    SPYT-775_useless_logs_in_sbt_test
    * Changelog entry
      Type: fix
      Component: spyt
    
    1) delete warns about non-existing sbt credentials
    2) change tech.ytsaurus.spyt.wrapper logging level to warn
    commit_hash:facc505a59c9da982fc75647e3b0c94ca261ae01

--- project/CommonPlugin.scala
+++ project/CommonPlugin.scala
@@ -24,48 +24,57 @@ object CommonPlugin extends AutoPlugin {
 
   import autoImport._
 
-  override def projectSettings: Seq[Def.Setting[_]] = Seq(
-    externalResolvers := Resolver.combineDefaultResolvers(resolvers.value.toVector, mavenCentral = false),
-    ivyConfigurations ++= Seq(SparkCompile, SparkRuntimeTest),
-    resolvers += Resolver.mavenLocal,
-    resolvers += Resolver.mavenCentral,
-    resolvers += ("YTsaurusSparkReleases" at "https://repo1.maven.org/maven2"),
-    resolvers += ("YTsaurusSparkSnapshots" at "https://s01.oss.sonatype.org/content/repositories/snapshots/"),
-    ThisBuild / version := (ThisBuild / spytVersion).value,
-    organization := "tech.ytsaurus",
-    name := s"spark-yt-${name.value}",
-    organizationName := "YTsaurus",
-    organizationHomepage := Some(url("https://ytsaurus.tech/")),
-    scmInfo := Some(ScmInfo(url("https://github.com/ytsaurus/ytsaurus"), "scm:git@github.com:ytsaurus/ytsaurus.git")),
-    developers := List(
-      Developer("YTsaurus", "YTsaurus SPYT development team", "dev@ytsaurus.tech", url("https://ytsaurus.tech/")),
-    ),
-    description := "YTsaurus SPYT",
-    licenses := List(
-      "The Apache License, Version 2.0" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")
-    ),
-    homepage := Some(url("https://ytsaurus.tech/")),
-    pomIncludeRepository := { _ => false },
-    scalaVersion := "2.12.15",
-    javacOptions ++= Seq("-source", "11", "-target", "11"),
-    publishTo := {
-      if (isSnapshot.value)
-        Some("snapshots" at "https://s01.oss.sonatype.org/content/repositories/snapshots/")
-      else
-        Some("releases" at "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-    },
-    publishMavenStyle := true,
-    credentials += Credentials(Path.userHome / ".sbt" / ".credentials"),
-    credentials += Credentials(Path.userHome / ".sbt" / ".ossrh_credentials"),
-    libraryDependencies ++= testDeps,
-    Test / fork := true,
-    printCompileClasspath := {
-      (Compile / dependencyClasspath).value.foreach(a => println(s"${a.metadata.get(configuration.key)} - ${a.data.getAbsolutePath}"))
-    },
-    printTestClasspath := {
-      (Test / fullClasspath).value.foreach(a => println(s"${a.metadata.get(configuration.key)} - ${a.data.getAbsolutePath}"))
-    },
-    Global / pgpPassphrase := gpgPassphrase.map(_.toCharArray),
-    ivyConfigurations := overrideConfigs(CompileProvided, TestProvided, NewCompileInternal, NewTestInternal)(ivyConfigurations.value),
-  )
+  override def projectSettings: Seq[Def.Setting[_]] = {
+    val credentialsDir = Path.userHome / ".sbt"
+    val credentialsFile = credentialsDir / ".credentials"
+    val ossrhCredentialsFile = credentialsDir / ".ossrh_credentials"
+
+    val credentialsSettings = Seq(
+      credentialsFile,
+      ossrhCredentialsFile
+    ).filter(_.exists()).map(Credentials(_))
+
+    Seq(
+      externalResolvers := Resolver.combineDefaultResolvers(resolvers.value.toVector, mavenCentral = false),
+      ivyConfigurations ++= Seq(SparkCompile, SparkRuntimeTest),
+      resolvers += Resolver.mavenLocal,
+      resolvers += Resolver.mavenCentral,
+      resolvers += ("YTsaurusSparkReleases" at "https://repo1.maven.org/maven2"),
+      resolvers += ("YTsaurusSparkSnapshots" at "https://s01.oss.sonatype.org/content/repositories/snapshots/"),
+      ThisBuild / version := (ThisBuild / spytVersion).value,
+      organization := "tech.ytsaurus",
+      name := s"spark-yt-${name.value}",
+      organizationName := "YTsaurus",
+      organizationHomepage := Some(url("https://ytsaurus.tech/")),
+      scmInfo := Some(ScmInfo(url("https://github.com/ytsaurus/ytsaurus"), "scm:git@github.com:ytsaurus/ytsaurus.git")),
+      developers := List(
+        Developer("YTsaurus", "YTsaurus SPYT development team", "dev@ytsaurus.tech", url("https://ytsaurus.tech/")),
+      ),
+      description := "YTsaurus SPYT",
+      licenses := List(
+        "The Apache License, Version 2.0" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")
+      ),
+      homepage := Some(url("https://ytsaurus.tech/")),
+      pomIncludeRepository := { _ => false },
+      scalaVersion := "2.12.15",
+      javacOptions ++= Seq("-source", "11", "-target", "11"),
+      publishTo := {
+        if (isSnapshot.value)
+          Some("snapshots" at "https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        else
+          Some("releases" at "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+      },
+      publishMavenStyle := true,
+      libraryDependencies ++= testDeps,
+      Test / fork := true,
+      printCompileClasspath := {
+        (Compile / dependencyClasspath).value.foreach(a => println(s"${a.metadata.get(configuration.key)} - ${a.data.getAbsolutePath}"))
+      },
+      printTestClasspath := {
+        (Test / fullClasspath).value.foreach(a => println(s"${a.metadata.get(configuration.key)} - ${a.data.getAbsolutePath}"))
+      },
+      Global / pgpPassphrase := gpgPassphrase.map(_.toCharArray),
+      ivyConfigurations := overrideConfigs(CompileProvided, TestProvided, NewCompileInternal, NewTestInternal)(ivyConfigurations.value)
+    ) ++ credentialsSettings.map(c => credentials += c)
+  }
 }

83a1fd0ee5175423d26ad22ad00a285bcfa154d4
Nikita Sokolov <faucct@tracto.ai>
2025-02-13 11:53:24 +0100

    table_writer configuration tests

--- data-source/src/main/scala/tech/ytsaurus/spyt/format/conf/YtTableSparkSettings.scala
+++ data-source/src/main/scala/tech/ytsaurus/spyt/format/conf/YtTableSparkSettings.scala
@@ -80,7 +80,7 @@ object YtTableSparkSettings {
 
   case object SortColumns extends ConfigEntry[Seq[String]]("sort_columns", Some(Nil))
 
-  case object WriteTableConfig extends ConfigEntry[YTreeNode](s"write_table_config")
+  case object TableWriterConfig extends ConfigEntry[YTreeNode]("table_writer")
 
   case object UniqueKeys extends ConfigEntry[Boolean]("unique_keys", Some(false))
 
@@ -113,7 +113,7 @@ object YtTableSparkSettings {
       }.getOrElse(str)
     }
 
-    val excludeOptions: Set[String] = Set(SortColumns, Schema, WriteTypeV3, NullTypeAllowed, Path).map(_.name)
+    val excludeOptions: Set[String] = Set(SortColumns, Schema, WriteTypeV3, NullTypeAllowed, Path, TableWriterConfig).map(_.name)
   }
 
   def isTable(configuration: Configuration): Boolean = {

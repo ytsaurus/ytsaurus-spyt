5b049338b0a1000e3681fcd1bf16f956be8ed8cb
mihailagei <mihailagei@ytsaurus.tech>
2025-05-21 16:35:24 +0300

    optimize latestOffset 2
    * Changelog entry
      Type: feature
      Component: spyt
    commit_hash:1228f4fac14cba115535b329f5d1c28b5ae74ac2

--- data-source/src/main/scala/tech/ytsaurus/spyt/streaming/YtStreamingSource.scala
+++ data-source/src/main/scala/tech/ytsaurus/spyt/streaming/YtStreamingSource.scala
@@ -13,9 +13,6 @@ import tech.ytsaurus.spyt.wrapper.YtWrapper
 import tech.ytsaurus.spyt.wrapper.client.YtClientConfigurationConverter.ytClientConfiguration
 import tech.ytsaurus.spyt.wrapper.client.YtClientProvider
 
-import scala.collection.SortedMap
-import scala.util.{Failure, Success}
-
 
 class YtStreamingSource(sqlContext: SQLContext,
                         consumerPath: String,
@@ -25,7 +22,6 @@ class YtStreamingSource(sqlContext: SQLContext,
 
   private implicit val yt: CompoundClient = YtClientProvider.ytClient(ytClientConfiguration(sqlContext.sparkSession))
   private val cluster = YtWrapper.clusterName()
-  private var latestOffset: Option[YtQueueOffset] = None
   private val includeServiceColumns = parameters.get("include_service_columns").exists(_.toBoolean)
 
   override def getDefaultReadLimit: ReadLimit = parameters.get("max_rows_per_partition") match {
@@ -34,46 +30,27 @@ class YtStreamingSource(sqlContext: SQLContext,
   }
 
   override def latestOffset(startOffset: streaming.Offset, limit: ReadLimit): streaming.Offset = {
-    val limits: Seq[ReadLimit] = limit match {
-      case rows: CompositeReadLimit => rows.getReadLimits
-      case rows => Seq(rows)
+    val maxRows = limit match {
+      case c: CompositeReadLimit => c.getReadLimits.collectFirst { case r: ReadMaxRows => r.maxRows() }
+      case r: ReadMaxRows => Some(r.maxRows())
+      case _ => None
     }
 
-    val maxOffsetInQueue = getOffset.get.asInstanceOf[YtQueueOffset]
-    val maxRowsOpt = limits.collectFirst { case rmr: ReadMaxRows => rmr }
-    if (maxRowsOpt.isDefined) {
-      val maxRows = maxRowsOpt.get.maxRows()
-      val startYtQueueOffset: YtQueueOffset = if (startOffset != null) YtQueueOffset(startOffset) else null
-      val partitionSeq = maxOffsetInQueue.partitions.toSeq.map { case (i, upperIndex) =>
-        val realStartIndex = startYtQueueOffset match {
-          case YtQueueOffset(_, _, partitions) => partitions.getOrElse(i, -1L)
-          case _ =>
-            val currentOffset = YtQueueOffset.getCurrentOffset(cluster, consumerPath, queuePath)
-            currentOffset.partitions.getOrElse(i, -1L)
-        }
+    val maxOffset = YtQueueOffset.getMaxOffset(cluster, queuePath).get
 
-        val endOffset = math.min(realStartIndex + maxRows, upperIndex)
-        (i, endOffset)
-      }
-      YtQueueOffset(cluster, queuePath, SortedMap(partitionSeq: _*))
-    } else {
-      maxOffsetInQueue
+    if (maxRows.isEmpty) {return maxOffset}
+
+    val startOffsetParsed = Option(startOffset).map(YtQueueOffset.apply)
+      .getOrElse(YtQueueOffset.getCurrentOffset(cluster, consumerPath, queuePath))
+
+    val partitions = maxOffset.partitions.map { case (i, upper) =>
+      val start = startOffsetParsed.partitions.getOrElse(i, -1L)
+      i -> math.min(start + maxRows.get, upper)
     }
+    YtQueueOffset(cluster, queuePath, partitions)
   }
 
-  override def getOffset: Option[Offset] = {
-    logDebug(s"Get offset for $queuePath")
-    val newOffset = YtQueueOffset.getMaxOffset(cluster, queuePath)
-    newOffset match {
-      case Success(value) =>
-        latestOffset = Some(value)
-        latestOffset
-      case Failure(exception) =>
-        // Fault-tolerance for temporal table issues.
-        logWarning("Error while getting new offset", exception)
-        latestOffset.orElse(throw new IllegalStateException("Latest and new offsets are lost"))
-    }
-  }
+  override def getOffset: Option[Offset] = None
 
   override def getBatch(start: Option[Offset], end: Offset): DataFrame = {
     val rdd = if (start.isDefined && start.get == end) {

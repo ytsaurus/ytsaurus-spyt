a6e85774db01b57d586672511bdaabc8efce4eee
keepling <keepling@ytsaurus.tech>
2025-11-09 03:56:26 +0300

    SPYT-826 Support docker with pyspark archive
    * Changelog entry
      Type:  feature
      Component: spyt
    
    Support SPYT docker with pyspark archive
    commit_hash:82d6db1f5084088d76c8b6285a52458c6683e45c

--- tools/release/spyt_image/build.sh
+++ tools/release/spyt_image/build.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 build_output=../../../build_output
 publish_scripts=../publisher
 image_cr="ghcr.io/"
+spark_version=""
 
 print_usage() {
     cat <<EOF
@@ -18,6 +19,7 @@ Usage: $script_name [-h|--help]
   --build-output: Path to built spyt components (default: $build_output)
   --publish-scripts: Path to spyt publish scripts (default: $publish_scripts)
   --spyt-version: Spyt version
+  --spark-version: Spark version (e.g., 3.5.7) to include as an archive. Requires Spark 3.4.0 or newer. (Optional)
   --image-cr: Image container registry (default: $image_cr)
 
 EOF
@@ -40,6 +42,10 @@ while [[ $# -gt 0 ]]; do
         spyt_version="$2"
         shift 2
         ;;
+        --spark-version)
+        spark_version="$2";
+        shift 2
+        ;;
         -h|--help)
         print_usage
         shift
@@ -56,6 +62,16 @@ if [[ -z "$spyt_version" ]]; then
   exit 1
 fi
 
+if [[ -n "$spark_version" ]]; then
+      if [[ "$spark_version" < "3.4.0" ]]; then
+          echo "Error: Spark version $spark_version is not supported. Only versions 3.4.0 or higher are supported." >&2
+          exit 1
+      fi
+    echo "Spark version to download: $spark_version"
+else
+    echo "Spark archive download: SKIPPED"
+fi
+
 mkdir data
 cp -rL $build_output/* data/
 mkdir scripts
@@ -66,6 +82,7 @@ docker build \
     -t ${image_cr}ytsaurus/spyt:"$spyt_version" \
     --build-arg BUILD_OUTPUT_PATH=data \
     --build-arg PUBLISH_SCRIPTS_PATH=scripts \
+    --build-arg SPARK_VERSION="$spark_version" \
     .
 
 rm -rf data

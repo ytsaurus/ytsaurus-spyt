be4112367646d9d1467d77393596a3176b08ff0a
keepling <keepling@ytsaurus.tech>
2026-04-09 11:25:42 +0300

    SPYT: improve deploy script
    * Changelog entry
      Type:  feature
      Component: spyt
    
    <Message for release notes>
    
    Modify deploy script to enable open source release mode
    commit_hash:57d8ea106bbf4e2297a39ac25c8d7635f4acb5b1

--- tools/release/deploy_snapshot.sh
+++ tools/release/deploy_snapshot.sh
@@ -3,13 +3,16 @@
 set -ex
 
 script_name=$0
+os_release=false
 
 print_usage() {
     cat <<EOF
 Usage: $script_name [-h|--help]
-                    [--proxy proxy address]
+                    [--proxy <proxy address>]
+                    [--os-release]
 
-  --proxy: YT proxy address
+  --proxy:       YT proxy address
+  --os-release:  use OS release mode (by default inner release mode is enabled.
 
 EOF
     exit 0
@@ -23,9 +26,12 @@ while [[ $# -gt 0 ]]; do
         proxy="$2"
         shift 2
         ;;
+        --os-release)
+        os_release=true
+        shift
+        ;;
         -h|--help)
         print_usage
-        shift
         ;;
         *)  # unknown option
         echo "Unknown argument $1"
@@ -49,5 +55,10 @@ trap 'rm -r $build_dir' EXIT
 
 cd "$script_dir"
 
-python3 -m publisher.config_generator build --inner-release
+config_generator_args=(build)
+if [[ "$os_release" != true ]]; then
+    config_generator_args+=(--inner-release)
+fi
+
+python3 -m publisher.config_generator "${config_generator_args[@]}"
 python3 -m publisher.publish_cluster build --ignore-existing

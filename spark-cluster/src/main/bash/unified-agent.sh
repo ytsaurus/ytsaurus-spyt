c18f4b8ae2b2306a13fd9d31201c81c5d4dd344d
smirkina <smirkina@ytsaurus.tech>
2026-05-08 17:01:11 +0300

    SPYT-1043 Update layer with unified agent
    * Changelog entry
      Type: feature
      Component: spyt
    
    Update layer with unified agent
    commit_hash:893be0d43db81d314605daa36cceb1d5d17262e0

--- spark-cluster/src/main/bash/unified-agent.sh
+++ spark-cluster/src/main/bash/unified-agent.sh
@@ -2,5 +2,13 @@
 
 set -ex
 
+KEYS='B8E34ED180EF6310 FF5F4D0E27393420'
+for key in $KEYS; do
+  curl -sL "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x${key}" | \
+    gpg --dearmor -o "/etc/apt/trusted.gpg.d/${key}.gpg"
+done
+
 apt-get update
+apt-get install libc6
 apt-get install -t stable -y yandex-unified-agent
+apt-get clean
